499b450439a8f1bdced986ff8da354f5de357ea9
03fred <fred.rocke02@gmail.com>
2019-07-05 10:30:10 -0300

    Ngclass

--- angular/src/app/diretivas/diretivas.component.ts
+++ angular/src/app/diretivas/diretivas.component.ts
@@ -6,9 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./diretivas.component.less']
 })
 export class DiretivasComponent implements OnInit {
- 
- cursosFor: string[] = ['Angular','Java','Phonegap'];
- 
+ aberto: boolean = false;
+
+ mudarAbertoFechado()
+{
+  this.aberto = !this.aberto;
+} 
   // aba = 'home';
 
 
@@ -20,17 +23,16 @@ export class DiretivasComponent implements OnInit {
   this.listaCursos[0] = 'angular';
   this.mostrarCursos = !this.mostrarCursos;
   }
+ cursosFor: string[] = ['Angular','Java','Phonegap'];
+  for(let i =0; i < this.cursosFor.length;i++){
+       let cursosFor = this.cursosFor[i];
+
+   }
 */
 
   constructor() { }
 
   ngOnInit() {
-
-   for(let i =0; i < this.cursosFor.length;i++){
-       let cursosFor = this.cursosFor[i];
-
-   }
-
   }
 
 }
